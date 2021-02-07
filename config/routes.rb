09a4eb7673675f402140a73c7b7001e7c761020b
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-07 14:23:10 -0500

    commit tree 28b47acb36beaa8a7f6e636143f9464fd8672aaf

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :students, only: [:index,:new,:create,:show,:edit]
+  patch 'student/:id/update', to: 'students#update', as: 'update_student'
+
+  resources :student_classes, only: [:index,:new,:create,:show,:edit]
+  patch 'student_class/:id/update', to: 'student_classes#update', as: 'update_student_class'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
